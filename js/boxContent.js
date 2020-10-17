f7c9f54afea8cfb8b61e5533fe531da1aef59778
somej123 <somej@naver.com>
2020-10-18 00:28:17 +0900

    box슬라이더 기능 완성

--- js/boxContent.js
+++ js/boxContent.js
@@ -2,12 +2,50 @@ $(document).ready(function(){
     var $box = $('.box_title').children('li');
     var $box_content = $('.se2_box').children('div');
     var now_num = 0;
+    var $count;
 
     $box.click(function(){
         console.log($box_content);
         now_num = $(this).index();
         $box.eq(now_num).addClass('on2').siblings().removeClass('on2');
-        $box_content.fadeOut();
-        $box_content.eq(now_num).fadeIn();
+        $box_content.stop().fadeOut();
+        $box_content.eq(now_num).stop().fadeIn();
     });
+
+    function init(){
+        $now_num = $('.se2_box>on').index();
+    }
+    function box_slider(){
+
+        function autoPlay(){
+            $count = setInterval(function(){
+                go();
+            },5000);
+        }
+
+        $('.se2_box').mouseenter(function(){
+            clearInterval($count);
+        });
+
+        $('.se2_box').mouseleave(function(){
+            autoPlay();
+        })
+
+        function go(){
+            if(now_num == 3){
+                now_num = 0;
+            }else{
+                now_num++;
+            }
+            Play();
+        }
+        function Play(){
+            $box.eq(now_num).addClass('on2').siblings().removeClass('on2');
+            $box_content.stop().fadeOut();
+            $box_content.eq(now_num).stop().fadeIn();
+        }
+        autoPlay();
+    }
+    init();
+    box_slider();
 });
